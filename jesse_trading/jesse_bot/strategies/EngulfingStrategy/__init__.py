da298e9a2d4cb47f6fa70d056e56fe61bc083425
zions <tzion12345@gmail.com>
2024-04-25 21:45:09 +0300

    engulfing improvements, 40% win-rate

--- jesse_trading/jesse_bot/strategies/EngulfingStrategy/__init__.py
+++ jesse_trading/jesse_bot/strategies/EngulfingStrategy/__init__.py
@@ -4,6 +4,15 @@ from jesse import utils
 import jesse.services.logger as logger
 import talib
 
+"""
+Rules: bullish engulfing candle when rsi is above 50 and price is above EMA 200
+Results: 
+    - win-rate: 35-42%
+    - tested between Feb-March 24, on largest 19 crypto pairs
+    - 1h, 4h timeframes showed same results
+    - support and resistance area may improve results
+"""
+
 
 OPEN_IDX = 1
 CLOSE_IDX = 2
@@ -16,7 +25,7 @@ class EngulfingStrategy(Strategy):
     def __init__(self):
         super().__init__()
         self.risk_amount = 50
-        self.proceed_stop = False
+        self.proceed_stop = True
         self.max_open_trades = False
 
     def should_long(self) -> bool:
@@ -35,12 +44,17 @@ class EngulfingStrategy(Strategy):
         # Rule 2: RSI is above 50
         rsi_above_midpoint = rsi_9[-1] > 50
         # Rule 3: Bullish engulfing candle
+        self.engulfing = talib.CDLENGULFING(self.candles[-2:, OPEN_IDX], self.candles[-2:, HIGH_IDX], self.candles[-2:, LOW_IDX], self.candles[-2:, CLOSE_IDX])
         engulfing_candle = current[CLOSE_IDX] >= previous[OPEN_IDX] > previous[CLOSE_IDX] >= current[OPEN_IDX]
-        # engulfing_candle = current[CLOSE_IDX] > previous[HIGH_IDX] and previous[LOW_IDX] >= current[OPEN_IDX] and previous[OPEN_IDX] > previous[CLOSE_IDX]
-        # Rule 4: TR is greater than 2 * ATR
+        engulfing_candle_no_eq = current[CLOSE_IDX] > previous[OPEN_IDX] > previous[CLOSE_IDX] > current[OPEN_IDX]
+        full_engulfing_candle = current[CLOSE_IDX] > previous[HIGH_IDX] and current[OPEN_IDX] < previous[LOW_IDX] and previous[OPEN_IDX] > previous[CLOSE_IDX]
+        if (engulfing_candle_no_eq and self.engulfing[-1] != 100):
+            logger.error(f"Mismatch of engulfing candle")
+        engulfing_candle_talib = self.engulfing[-1] == 100
+        # Rule 4: candle is big but not too big
         atr = ta.atr(self.candles, period=30)
         tr = ta.trange(self.candles)
-        big_candle = tr > 2 * atr
+        right_size_candle = 2 * atr < tr #< 4 * atr
 
         # Prices
         self.stop_length = 1 * (self.high - self.low)
@@ -49,7 +63,7 @@ class EngulfingStrategy(Strategy):
         position_size = self.qty * self.price
 
         # Entry Rule
-        if uptrend and rsi_above_midpoint and engulfing_candle and big_candle:
+        if uptrend and rsi_above_midpoint and engulfing_candle and right_size_candle:
             if position_size > 0.4 * self.portfolio_value:
                 logger.error(f"Position size of {self.symbol} is too large: {position_size}")
                 return False
@@ -80,5 +94,10 @@ class EngulfingStrategy(Strategy):
     def update_position(self):
         # proceed stop to entry price after movement of 1-risk
         if self.proceed_stop:
-            if self.price - self.position.entry_price >= self.stop_length:
+            if self.price - self.position.entry_price >= self.stop_length * 1.2:
                 self.stop_loss = self.position.qty, self.position.entry_price
+
+        if self.engulfing[-1] == -100:
+            new_stop_loss = self.candles[-2][LOW_IDX]
+            logger.info(f"Bearish engulfing candle detected - setting stop to {new_stop_loss}")
+            self.stop_loss = new_stop_loss
